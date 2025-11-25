3c2cb013effcc05f7b3b3218137f9db89220a014
Mon711 <mrinalsood711@gmail.com>
2025-11-25 21:09:22 +0530

    style FaqSection and FaqItem components

--- src/lib/components/FaqItem.svelte
+++ src/lib/components/FaqItem.svelte
@@ -1,13 +1,63 @@
 <script>
-    import chevronDown from "$assets/icons/chevron-down.svg"
+	import chevronDown from '$assets/icons/chevron-down.svg';
 </script>
 
 <button class="container">
-    <div class="question-and-answer">
-        <p class="question mb-s">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, odit?</p>
-        <p class="answer">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium explicabo et deleniti, ullam ipsum reiciendis assumenda odio omnis numquam impedit provident quaerat eius autem nostrum quia beatae iusto aut suscipit voluptatem, error itaque fugiat repellendus aperiam. Rem obcaecati doloremque, neque tempora iste est quas dignissimos dolores accusamus aperiam, qui voluptas.
-        </p>
-    </div>
-    <img src="{chevronDown}" alt="" />
+	<div class="question-and-answer">
+		<p class="question mb-s">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, odit?
+		</p>
+		<p class="answer">
+			Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium explicabo et deleniti,
+			ullam ipsum reiciendis assumenda odio omnis numquam impedit provident quaerat eius autem
+			nostrum quia beatae iusto aut suscipit voluptatem, error itaque fugiat repellendus aperiam.
+			Rem obcaecati doloremque, neque tempora iste est quas dignissimos dolores accusamus aperiam,
+			qui voluptas.
+		</p>
+	</div>
+	<img src={chevronDown} alt="" />
 </button>
+
+<style>
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: flex-start;
+		padding: 12px 0;
+		border-bottom: 1px solid grey;
+		text-align: left;
+	}
+
+	.question-and-answer {
+		padding-right: 150px;
+		flex-grow: 1;
+	}
+
+	.answer {
+		max-height: 0;
+		overflow: hidden;
+		opacity: 0;
+		transition:
+			max-height 0.3s ease-out,
+			opacity 0.3s ease-out;
+	}
+
+	button img {
+		width: 24px;
+		height: 24px;
+		transition: transform 0.3s linear;
+	}
+
+	.question {
+		font-weight: 600;
+	}
+
+	.container-expanded img {
+		transform: rotate(180deg);
+	}
+
+	.container-expanded .answer {
+		max-height: unset;
+		opacity: 1;
+	}
+</style>
--- src/lib/components/FaqSection.svelte
+++ src/lib/components/FaqSection.svelte
@@ -1,20 +1,38 @@
 <script>
 	import { FaqItem, Button } from '$components';
-	import twitterIcon from '$assets/icons/x-icon.webp';
+	import xIcon from '$assets/icons/x-icon.webp';
 </script>
 
-<section class="faq-section">
+<section class="landing-page-section">
 	<h2 class="mb-l">Frequently asked questions</h2>
 	<div class="faq-container">
 		<FaqItem />
 	</div>
-	<div class="additional-info">
+	<div class="additional-info mt-m">
 		<Button class="mt-s">BUY NOW</Button>
 		<p class="mt-m">Any other questons?</p>
 		<a href="https://x.com/MrinalSood3">
 			<p>
-				Hit me up on X <img src={twitterIcon} class="twitter-icon" alt="" />
+				Hit me up on <img src={xIcon} class="x-icon" alt="" />
 			</p>
 		</a>
 	</div>
 </section>
+
+<style>
+    .additional-info{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: grey;
+    }
+
+    .x-icon {
+        width: 16px;
+        height: 16px;
+    }
+
+    a{
+        text-decoration: underline;
+    }
+</style>
